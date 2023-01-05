da5fdea18e80fdc4573283b8c09a00b8dcf957b1
almir <madarat87@gmail.com>
2023-01-05 17:24:16 +0300

    created user table and properties for db work

---
+++ src/main/java/com/halal/boot3security6jwt/Boot3security6JwtApplication.java
@@ -0,0 +1,13 @@
+package com.halal.boot3security6jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot3security6JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot3security6JwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/halal/boot3security6jwt/Boot3security6JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halal.boot3security6jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot3security6JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
