adf43498554e80a34217e88067a9ab2c8af4c104
almir <madarat87@gmail.com>
2023-01-08 18:03:29 +0300

    ready auth controller and needed entities

---
+++ src/main/java/com/halal/boot3security6jwt/models/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.halal.boot3security6jwt.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
---
+++ src/main/java/com/halal/boot3security6jwt/services/AuthenticationService.java
@@ -0,0 +1,53 @@
+package com.halal.boot3security6jwt.services;
+
+import com.halal.boot3security6jwt.models.AuthenticationRequest;
+import com.halal.boot3security6jwt.models.AuthenticationResponse;
+import com.halal.boot3security6jwt.models.RegisterRequest;
+import com.halal.boot3security6jwt.repository.UserRepository;
+import com.halal.boot3security6jwt.user.Role;
+import com.halal.boot3security6jwt.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        User user = User.builder()
+                .firstname(request.getFirstName())
+                .lastname(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new AuthenticationServiceException("User with this email is not found"));
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
